ee4e698163383c4790ab19d50153287c99f366f1
Vladimir Chernyavskii <vladimir151055@gmail.com>
2025-01-30 17:08:45 +0600

    feat: added scripts for adding and deleting cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,27 @@
+const cardContainer = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+
+function createCard(title, link, deleteHandler) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__title").textContent = title;
+  cardElement.querySelector(".card__image").src = link;
+
+  deleteButton.addEventListener("click", deleteHandler);
+
+  cardContainer.append(cardElement);
+}
+
+function deleteCard(event) {
+  const listItem = event.target.closest(".places__item.card");
+  listItem.remove();
+}
+
+initialCards.forEach((element) =>
+  createCard(element.name, element.link, deleteCard)
+);
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
